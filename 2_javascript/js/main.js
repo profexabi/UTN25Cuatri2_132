18437918d50a25983ce2ad39d92b304d583a75ab
javie.rodriguez@bue.edu.ar <javiergallard91@gmail.com>
2025-10-15 12:39:04 -0300

    Engadido JavaScript VII

--- 2_javascript/js/main.js
+++ 2_javascript/js/main.js
@@ -1,264 +1,122 @@
-/* ====================================
-    DOM en JavaScript
-=======================================
+/*=========================
+    Callbacks
+===========================
 
-El DOM (Document Object Model) o Modelo de Objeto de Documento, es una interfaz de programacion que representa un documento HTML como una estructura jerarquica de objetos, conocida comunmente como arbol DOM.
+Los callbacks son funciones que se pasan como argumentos a otras funciones y se ejecutan despues de que ocurra algun evento o se complete alguna operacion
+*/
+let botonCoso;
 
-Esta estructura permite a JavaScript acceder, modificar o añador elementos, contenido, estilos y atributos de forma dinamica.
-
-Cada elemento HTML se convierte en un nodo dentro de este arbol y todos los elementos estan relacionados entre si mediante padres, hijos y hermanos, creando una representacion en memoria del documento que el navegador pueda manipular.
-
-Transforma el HTML en una estructura de nodos y objetos que puede ser manipulado con JavaScript. Cada etiqueta HTML es un nodo en el DOM.
-
-
-Ejemplo de estructura DOM__________
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Mi página</title>
-    </head>
-    <body>
-        <h1>Bienvenidos</h1>
-        <p>Este es un párrafo</p>
-    </body>
-</html>
-
-
-
-Este HTML seria representado en el DOM como una estructura en forma de arbol.
-document es el objeto que representa toda la pagina web
-
-Diagrama de arbol del DOM__________
-
-- document
-    - html
-        - head
-            - title
-        - body
-            - h1
-            - p
-
-
-Como funciona la manipulacion del DOM?
-
-JavaScriptt puede acceder y modificar cualquier elemento del DOM, utilizando el objeto global document.
-
-JavaScript puede
-    - Modificar el contenido (textos, atributos, clases)
-    - Añadir o eliminar elementos del DOM
-    - Escuchar eventos de usuario (clicks, teclas, etc) 
-    
-    
-============================
-    Seleccionar elementos   
-============================
-
-- getElementById():         Selecciona un elemento por su Id. Este es el metodo clave que usamos siempre
-- querySelector():          Selecciona el primer elemento que coincida con un selector CSS
-- querySelectorAll():       Selecciona todos los elementos que coincidan con un selector CSS
-
-- getElementByClassName():  Selecciona todos los elementos que tengan una clase especifica
-- getElementsByTagName():   Selecciona todos los elementos de un tipo de etiqueta dado
+/* Ejemplo de callback que veiamos en JavaScript VI
+botonCoso.addEventListener("keyup", function(event) {
+    console.log(event.key);
+});
 */
 
+// Ejemplo 1
+function saludar(nombre, callback) {
+    console.log(`Hola ${nombre}`);
+    // Podemos realizar otras cosas
 
-// getElementById()
-// Este metodo selecciona un unico elemento por su id. Si no lo encuentra, devuelve null
+    callback(); // Aca invocamos la funcion que pasamos como argumento
+}
 
-let titulo = document.getElementById("titulo");
-console.log(titulo); // <h1 id="titulo">Titulo principal</h1>
-console.log(titulo.textContent); // Titulo principal
+function despedirse() {
+    console.log("Chau, nos vemos");
+}
 
-
-// querySelector() y querySelectorAll()
-// querySelector: Selecciona el PRIMER elemento que coincida con un selector CSS (#nombreId, .nombreClase, etiqueta)
-// querySelectorAll: Selecciona TODOS los elementos que coincidan con el selector CSS y devuelve una NodeList (similar a un array
-
-// let primerParrafo = document.querySelector("#primerParrafo");
-let primerParrafo = document.querySelector(".mensaje");
-console.log(primerParrafo.textContent);
-
-let parrafos = document.querySelectorAll(".mensaje");
-console.log(parrafos); // NodeList(2) [p#primerParrafo.mensaje, p.mensaje]
-
-parrafos.forEach(par => console.log(par.textContent));
+saludar("Emiliano", despedirse); 
+// Hola Emiliano
+// Chau, nos vemos
 
 
+// Ejemplo 2
+function procesarDatos(datos, callback) {
+    console.log("Procesando datos...");
 
-// Cambiar el texto del parrafo
-let parrafo = document.querySelector("#parrafo");
+    let resultado = datos.toUpperCase();
 
-parrafo.textContent = "Soy el nuevo texto dinamico introducido desde JavaScript";
+    callback(resultado); // Le pasamos a la funcion los datos en mayuscula como argumento
+}
 
-parrafo.innerHTML = "Y aca me volvieron a cambiar <strong>zarpado man</strong>";
-
-
-// Modificar atributos
-let boton = document.getElementById("boton");
-
-boton.setAttribute("id", "nuevoId");
-
-boton.style.backgroundColor = "green";
-
-boton.style.color = "white";
-
-boton.style.padding = "10px";
+procesarDatos("hola mundo", (resultado) => {
+    console.log("Resultado: ", resultado);
+});
 
 
-/* Alternativa mas segura a innerHTML
-
-Podemos crear, insertar o eliminar elementos del DOM sin usar innerHTML y sin la posibilidad de poder introducir srcipts
-
-- createElement():  Crean un nuevo elemento HTML
-- appendChild():    Añaden un elemento como hijo de otro
-- removeChild():    Elminina un elemento hijo de su nodo padre
-- textContent():    Añade un contenido de texto plano
+// Ejemplo 3 con un temporizador
+setTimeout(() => {
+    console.log("Esto se ejecuta despues de 1 segundo");
+}, 1000);
 
 
-Comparacion
+/* Caracteristicas principales
 
-Con innerHTML______________________________________________
-document.getElementById("container").innerHTML = "<p>Hola mundo</p>";
+1. Funciones de primera clase
 
-    - Tecnicamente, innerHTML puede ejecutar scripts maliciosos si usamos datos del usuario sin validar
+En JavaScript, las funciones son tratadas como ciudadanos de primera clase "first class citizens", lo que significa que pueden ser:
 
-
-Con createElement + appendChild____________________________
-const p = document.createElement("p")
-p.textContent = "Hola mundo";
-document.getElementById("container").appendChild(p)
-
-    - createElement y appendChild no interpretan HTML y evitan inyecciones de codigo por defecto, por eso son mas seguro
+    - Asignadas a variables
+    - Pasadas como argumentos
+    - Retornadas desde otras funciones
 */
 
+// Asignar funcion a variable
+let miCallback = function() {
+    console.log("Callback ejecutado");
+}
+
+// Pasamos como argumento
+function ejecutarCallback(callback) {
+    callback(); // Invocamos la funcion que consologuea "Callback ejecutado"
+}
+
+ejecutarCallback(miCallback); // Callback ejecutado
 
 
+// Callback sincrono -> Traba el hilo de ejecucion principal hasta que terminen las 50.000 vueltas de bucle
+/*
+function procesoPesado(callback) {
+    console.log("Iniciando proceso...");
 
-/*==========================
-    Eventos en JavaScript
-============================
+    // Simulamos un procesamiento pesado
+    for (let i = 0; i < 50000; i++) {
+        console.log(`Iteracion: ${i}`)
+    }
 
-Los eventos en JavaScript permiten a los desarrolladores detectar interacciones del usuario con la pagina web, como hacer click en un boton, mover el mouse, escribir en un campo de texto, etc
-
-Los eventos son fundamentales para hacer que la pagina sea interactiva.
-
-Un evento es una señal que se envia cuando ocurre una interaccion o cambio en el documento, como un click, una pulsacion de tecla, etc.
-
-JavaScript permite escuchar estos eventos y ejecutar funciones especificas cuando ocurren
-
-
-- Eventos de mouse:         click, dbclick, mouseover, mouseout, mousemove
-
-- Eventos de teclado:       keydown, keyup
-
-- Eventos de formulario:    submit, change, input, focus
-
-- Eventos de ventana:       resize, scroll, load, unload
-*/
-
-
-let botonClickeable = document.getElementById("botonClickeable");
-
-// Escuchamos el evento click
-botonClickeable.addEventListener("click", function() {
-    console.log("Holis! Soy un mensaje por consola");
-});
-
-
-
-// Vamos a registrar las teclas pulsadas
-let input = document.getElementById("input");
-
-// Registramos por consola el valor del campo de texto
-input.addEventListener("keyup", function() {
-    // console.log(input.value); 
-});
-
-
-// Registramos por consola cada tecla que pulsamos
-// El objeto global event, contiene todos los datos del evento y nos permite acceder a la informacion del evento que fue disparado
-
-input.addEventListener("keydown", function(event) {
-    console.log(`Tecla presionada: ${event.key}`);
-    console.log(`Clave de la tecla presionada: ${event.code}`);
-});
-
-
-
-/*==============================
-    Propagacion de eventos
-================================
-
-Cuando ocurre un evento, este se propaga a traves del DOM en dos fases:
-    
-    - Fase de captura: De arriba para abajo
-    - Fase de burbuja: De abajo para arriba
-
-Podemos detener la propagacion de un evento usando el metodo event.stopPropagation()
-
-El metodo event.preventDefault() se usa para evitar el comportamiento predeterminado de un elemento. Por ejemplo, evitar que un formulario se envie al hacer click en el boton submit
-*/
-
-//////////////////////////////
-// event.stopPropagation() //
-
-let padre = document.getElementById("padre");
-let hijo = document.getElementById("hijo");
-
-// Escuchamos el click en el div padre 
-padre.addEventListener("click", function() {
-    console.log("Se hizo click en el div padre");
-});
-
-// Escuchamos el click en el boton hijo
-hijo.addEventListener("click", function(event) {
-    event.stopPropagation(); // Evitamos la propagacion de eventos
-    console.log("Se hizo click en el boton hijo");
-});
-
-
-
-/////////////////////////////
-// event.preventDefault() //
-
-let formulario = document.getElementById("formulario");
-
-// Evitamos que el formulario se envie por defecto
-formulario.addEventListener("submit", function(event) {
-    event.preventDefault();
-    console.log("Formulario no enviado!");
-});
-
-
-// EXTRA, renderizando una lista en el HTML
-
-const usuariosWeb = [
-    {id: 1, nombre: "Gricel", activo: true, rol: "user"},
-    {id: 2, nombre: "Francisco", activo: false, rol: "admin"},
-    {id: 3, nombre: "Leon", activo: true, rol: "user"}
-];
-
-// Arranco definiendo el contenedor de una lista HTML
-let contenedorLista = document.getElementById("contenedorLista");
-let listadoHTML = "<ul>";
-
-usuariosWeb.forEach(user => {
-    listadoHTML += `<li>id: ${user.id} / nombre: ${user.nombre} / rol: ${user.rol}</li>`;
-});
-
-listadoHTML += "</ul>";
-
-console.log(listadoHTML);
-
-// Renderizamos nuestro choclo HTML en formato string en nuestro document
-let mostrarLista = document.getElementById("mostrarLista");
-
-mostrarLista.addEventListener("click", renderizarLista);
-
-function renderizarLista() {
-    contenedorLista.innerHTML = listadoHTML;
+    callback(); // Aca se imprime "Proceso asincrono completado"
 }
 
 
-// Propuesta de portfolio, hagan una calculadora web!
+procesoPesado(function() {
+    console.log("Proceso asincrono completado");
+});
+
+
+console.log("Esto se ejecuta despues del callback sincrono"); // Despues de 50.000 iteraciones, se imprime esto
+*/
+
+
+
+// Callback asincrono -> No detiene el hilo de ejecucion princpal
+function procesoAsincrono(callback) {
+    console.log("Iniciando proceso asincrono...");
+
+    setTimeout(function() {
+        callback()
+    }, 2000);
+}
+
+
+procesoAsincrono(function() {
+    console.log("Proceso asincrono de 2segs completado");
+});
+
+console.log("Esto se ejecuta inmediatamente");
+
+
+/*========================================
+    Casos de usos comunes de callbacks
+==========================================
+
+
+*/
--- README.md
+++ README.md
@@ -13,6 +13,140 @@
 
 ---
 
+## Notas
+- Continuar JavaScript VII desde los casos de uso comunes de callbacks
+
+---
+
+
+## JavaScript VII / Callbacks, High order functions, asincronia, promesas, async await, destructuring, spread operator,  funciones anidadas, closures, web apis
+
+```js
+/*=========================
+    Callbacks
+===========================
+
+Los callbacks son funciones que se pasan como argumentos a otras funciones y se ejecutan despues de que ocurra algun evento o se complete alguna operacion
+*/
+let botonCoso;
+
+/* Ejemplo de callback que veiamos en JavaScript VI
+botonCoso.addEventListener("keyup", function(event) {
+    console.log(event.key);
+});
+*/
+
+// Ejemplo 1
+function saludar(nombre, callback) {
+    console.log(`Hola ${nombre}`);
+    // Podemos realizar otras cosas
+
+    callback(); // Aca invocamos la funcion que pasamos como argumento
+}
+
+function despedirse() {
+    console.log("Chau, nos vemos");
+}
+
+saludar("Emiliano", despedirse); 
+// Hola Emiliano
+// Chau, nos vemos
+
+
+// Ejemplo 2
+function procesarDatos(datos, callback) {
+    console.log("Procesando datos...");
+
+    let resultado = datos.toUpperCase();
+
+    callback(resultado); // Le pasamos a la funcion los datos en mayuscula como argumento
+}
+
+procesarDatos("hola mundo", (resultado) => {
+    console.log("Resultado: ", resultado);
+});
+
+
+// Ejemplo 3 con un temporizador
+setTimeout(() => {
+    console.log("Esto se ejecuta despues de 1 segundo");
+}, 1000);
+
+
+/* Caracteristicas principales
+
+1. Funciones de primera clase
+
+En JavaScript, las funciones son tratadas como ciudadanos de primera clase "first class citizens", lo que significa que pueden ser:
+
+    - Asignadas a variables
+    - Pasadas como argumentos
+    - Retornadas desde otras funciones
+*/
+
+// Asignar funcion a variable
+let miCallback = function() {
+    console.log("Callback ejecutado");
+}
+
+// Pasamos como argumento
+function ejecutarCallback(callback) {
+    callback(); // Invocamos la funcion que consologuea "Callback ejecutado"
+}
+
+ejecutarCallback(miCallback); // Callback ejecutado
+
+
+// Callback sincrono -> Traba el hilo de ejecucion principal hasta que terminen las 50.000 vueltas de bucle
+/*
+function procesoPesado(callback) {
+    console.log("Iniciando proceso...");
+
+    // Simulamos un procesamiento pesado
+    for (let i = 0; i < 50000; i++) {
+        console.log(`Iteracion: ${i}`)
+    }
+
+    callback(); // Aca se imprime "Proceso asincrono completado"
+}
+
+
+procesoPesado(function() {
+    console.log("Proceso asincrono completado");
+});
+
+
+console.log("Esto se ejecuta despues del callback sincrono"); // Despues de 50.000 iteraciones, se imprime esto
+*/
+
+
+
+// Callback asincrono -> No detiene el hilo de ejecucion princpal
+function procesoAsincrono(callback) {
+    console.log("Iniciando proceso asincrono...");
+
+    setTimeout(function() {
+        callback()
+    }, 2000);
+}
+
+
+procesoAsincrono(function() {
+    console.log("Proceso asincrono de 2segs completado");
+});
+
+console.log("Esto se ejecuta inmediatamente");
+
+
+/*========================================
+    Casos de usos comunes de callbacks
+==========================================
+
+
+*/
+```
+
+---
 
 ## JavaScript VI / Manipulacion del DOM en JavaScript y Eventos
 ```js
@@ -1955,9 +2089,5 @@ console.log(e);
 
 ## JavaScript VIII / JSON, asincronia, promesas, fetch, async/await y try/catch
 
----
 
-## JavaScript VII / High order functions, destructuring, spread operator, closures, funciones anidadas, callbacks, web apis
-
----
 
