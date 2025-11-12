fca09ed9f5e04a2d5dd70488d460dac3e14f150b
javie.rodriguez@bue.edu.ar <javiergallard91@gmail.com>
2025-11-12 11:36:44 -0300

    Engadidos endpoints PUT, DELETE e vistas

--- 5_tpIntegradorBack/index.js
+++ 5_tpIntegradorBack/index.js
@@ -40,7 +40,7 @@ Sin este permiso explícito, el navegador bloquea la solicitud para mantener la 
 
 El cuerpo de la solicitud, disponible en `req.body`, se utiliza comúnmente para recibir datos enviados en peticiones POST o PUT, aunque requiere middleware como `express.json()` para ser procesado correctamente
 */
-app.use(express.json());
+app.use(express.json()); // Middleware que convierte los datos "application/json" que nos proporciona la cabecera (header) de las solicitudes POST y PUT, los pasa de json a objetos JS
 
 
 /*===================
@@ -48,8 +48,16 @@ app.use(express.json());
 ===================*/
 
 app.get("/", (req, res) => {
+    // Tipo de respuesta texto plano
     res.send("TP Integrador Div 132");
 });
+
+
+/* Devolveremos vistas
+app.get("/dashboard", (req, res) => {
+    res.render("index");
+});
+*/
 
 
 // GET all products -> Traer todos los productos
@@ -60,6 +68,7 @@ app.get("/api/products", async (req, res) => {
         // la conexion devuelve dos campos, rows con el resultado de la consulta, fields la informacion de la tabla products
         const [rows, fields] = await connection.query(sql);
 
+        // Tipo de respuesta en JSON
         res.status(200).json({
             payload: rows,
             message: "Productos encontrados"
@@ -140,10 +149,74 @@ app.post("/api/products", async (req, res) => {
 
 // PUT-> Actualizar producto
 app.put("/api/products", async (req, res) => {
+    try {
+        /*{
+            "id": "2",
+            "name": "hamburguesa salmon",
+            "image": "https://burgernj.com/wp-content/uploads/2021/05/Salmon-Burger_.jpg",
+            "category": "food",
+            "price": "2500.00",
+            "active": "1"
+        }*/
 
+        // Gracias al middleware express.json() convertimos el JSON previo en un objeto JS al que podemos hacer destructuring y almacenar en variables sus valores
+        let { id, name, image, category, price, active } = req.body;
+
+        let sql = `
+            UPDATE products
+            SET name = ?, image = ?, category = ?, price = ?, active = ?
+            WHERE id = ?
+        `;
+
+        let [result] = await connection.query(sql, [name, image, category, price, active, id]); // Estos valores en orden reemplazan a los placeholders -> ?
+
+        console.log(result);
+
+        res.status(200).json({
+            message: `Producto con id: ${id} actualizado correctamente`
+        });
+
+    } catch(error) {
+        // Vemos el error en la consola del servidor
+        console.error("Error al actualizar producto: ", error);
+
+        // Le respondemos al cliente con un codigo 500 y un mensaje de error
+        res.status(500).json({
+            message: `Error interno del servidor: ${error}`
+        });
+    }
 });
 
-// TO DO, crear middleware logger (middleware de aplicacion) y validateID (middleware de ruta)
+
+
+// DELETE-> Eliminar producto
+app.delete("/api/products/:id", async (req, res) => {
+    try {
+        let { id } = req.params;
+
+        // Opcion 1: Hacer el borrado normal
+        let sql = `DELETE FROM products WHERE id = ?`;
+
+        // Opcion 2: Baja logica
+        let sql2 = `UPDATE products set active = 0 WHERE id = ?`;
+
+        let [result] = await connection.query(sql, [id]);
+        console.log(result);
+
+        return res.status(200).json({
+            message: `Producto con id ${id} eliminado correctamente`
+        });
+
+    } catch (error) {
+        console.error("Error al eliminar un producto por su id: ", error);
+
+        res.status(500).json({
+            message: `Error al eliminar producto con id: ${id}`,
+            error: error.message
+        });
+    }
+});
+
 
 
 
